6d28e67c5f16286f57c4237121d83f9ffcb0fada
dnwns3437 <chorjgml@naver.com>
2024-05-13 19:03:50 +0900

    .
    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,8 +61,12 @@ def is_user_valid(username, userID):
     else:
         user = return_user_with_name(username + " " + userID)
         if user == None:
-            st.error("User is invalid or no in100 rank data found")
-            st.stop()
+            if list((search_user(("username"), exact = True)).values())[0] != 0:
+                st.error("User ID is invalid. Please provide the correct ID")
+                st.stop()
+            else:
+                st.error("User is invalid or no in100 rank data found")
+                st.stop()
         else:
             st.success("User is valid")
     return user
