3e241b379fec02fc57f158d9f345ce252ee0016f
dnwns3437 <chorjgml@naver.com>
2024-05-13 17:59:48 +0900

    .
    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,15 @@ import streamlit as st
 st.header("PIU in 100 data Analyzer")
 st.write("This is a simple web app that analyzes the data from the PIU in 100 rank dataset.")
 st.write("Currently the data is up to 05/09/2024")
+
+
+main_tabs = st.tabs["Readme", "Single Player Analysis", "Player comaparison", "updates"]
+
+if main_tabs == "Readme":
+    st.write("리드미는나중에")
+elif main_tabs == "Single Player Analysis":
+    st.write("This is the single player analysis page")
+elif main_tabs == "Player comaparison":
+    st.write("This is the player comparison page")
+elif main_tabs == "updates":
+    st.write("This is the update page")
